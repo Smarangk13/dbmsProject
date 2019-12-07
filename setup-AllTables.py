d7eeee924904f9b471d3d73a134daf18c491e48f
Smarangk13 <smarangk@gmail.com>
2019-12-07 15:03:13 -0500

    added functions to calss

--- setup-AllTables.py
+++ setup-AllTables.py
@@ -1,98 +1,101 @@
 import sqlite3
 
-conn = sqlite3.connect("CS579.db")
-cursor = conn.cursor()
 
-SQLcommands = []
+class setup:
+    def __init__(self):
+        self.conn = sqlite3.connect("CS579.db")
+        self.cursor = self.conn.cursor()
 
-def createTable(tableName,createCommand, index = None, indexedValue = None):
-    global SQLcommands
-    global cursor
-    dropTable = 'DROP TABLE IF EXISTS ' + tableName
-    cursor.execute(dropTable)
+        self.SQLcommands = []
 
-    SQLcommands.append(createCommand)
+    def createTable(self, tableName, createCommand, index = None, indexedValue = None):
+        dropTable = 'DROP TABLE IF EXISTS ' + tableName
+        self.cursor.execute(dropTable)
 
-    if index is not None:
-        priceIndex = 'CREATE INDEX' + index + 'ON' + tableName+"("+indexedValue+")"
-        SQLcommands.append(priceIndex)
+        self.SQLcommands.append(createCommand)
 
-def executeStatements():
-    global SQLcommands
-    global cursor
-    global conn
+        if index is not None:
+            indexCommand = 'CREATE INDEX ' + index + ' ON ' + tableName+" ("+indexedValue+")"
+            self.SQLcommands.append(indexCommand)
 
-    for table in SQLcommands:
-        cursor.execute(table)
-    conn.commit()
+    def executeStatements(self):
+        for command in self.SQLcommands:
+            # Added print to get all sql statements used(project deliverable 4)
+            print(command)
+            self.cursor.execute(command)
+        self.conn.commit()
 
-carsTable = """CREATE TABLE Car (
-                CarVIN Integer primary key, 
-                Model, 
-                Make text, 
-                Year text, 
-                Color text, 
-                Value real, 
-                Engine text, 
-                VendorID Integer, 
-                PurchasePrice real, 
-                PurchaseDate text
-                )"""
 
-vendorTable = """CREATE TABLE Vendor (
-                VendorID Integer primary key, 
-                Name text text, 
-                Address text, 
-                PhoneNumber text, 
-                VendorType text
-                )"""
+if __name__ == '__main__':
+    dbManager = setup()
+    carsTable = """CREATE TABLE Car (
+                    CarVIN Integer primary key, 
+                    Model, 
+                    Make text, 
+                    Year text, 
+                    Color text, 
+                    Value real, 
+                    Engine text, 
+                    VendorID Integer, 
+                    PurchasePrice real, 
+                    PurchaseDate text
+                    )"""
 
-salesTable = """CREATE TABLE Sales (
-                TransactionID Integer primary key, 
-                StaffID Integer, 
-                SCarVIN Integer, 
-                CustomerName text, 
-                CustomerPhone Integer, 
-                CustomerAddress text, 
-                SalePrice real
-                )"""
+    vendorTable = """CREATE TABLE Vendor (
+                    VendorID Integer primary key, 
+                    Name text text, 
+                    Address text, 
+                    PhoneNumber text, 
+                    VendorType text
+                    )"""
 
-soldTable = """CREATE TABLE soldCars (
-                SCarID Integer primary key, 
-                DateOfSale text
-                )"""
+    salesTable = """CREATE TABLE Sales (
+                    TransactionID Integer primary key, 
+                    StaffID Integer, 
+                    SCarVIN Integer, 
+                    CustomerName text, 
+                    CustomerPhone Integer, 
+                    CustomerAddress text, 
+                    SalePrice real
+                    )"""
 
-onHandTable = """CREATE TABLE onHandCars (
-                OCarID Integer primary key, 
-                Availibility text
-                )"""
+    soldTable = """CREATE TABLE soldCars (
+                    SCarID Integer primary key, 
+                    DateOfSale text
+                    )"""
 
-staffTable = """CREATE TABLE Staff (
-                StaffID Integer primary key, 
-                Name text, 
-                Role text, 
-                Salary real, 
-                DateofEmployment text, 
-                DateofBirth text, 
-                Address text, 
-                StaffPhone Integer
-                )"""
+    onHandTable = """CREATE TABLE onHandCars (
+                    OCarID Integer primary key, 
+                    Availability text
+                    )"""
 
-maintenanceRecordTable = """CREATE TABLE MaintenanceRecord (
-                ServiceNumber Integer primary key, 
-                CarVIN Integer, 
-                DateofService text, 
-                Expenditure real, 
-                ChargetoCustomer real, 
-                NextServiceDate text
-                )"""
+    staffTable = """CREATE TABLE Staff (
+                    StaffID Integer primary key, 
+                    Name text, 
+                    Role text, 
+                    Salary real, 
+                    DateOfEmployment text, 
+                    DateOfBirth text, 
+                    Address text, 
+                    StaffPhone Integer
+                    )"""
 
-createTable('Car',carsTable,'CostIndex','PurchasePrice')
-createTable('Vendor',vendorTable)
-createTable('Sales',salesTable,'SalesIndex','SalePrice')
-createTable('soldCars',soldTable)
-createTable('onHandCars',onHandTable)
-createTable('Staff',staffTable,'PayIndex','Salary')
-createTable('MaintenanceRecord',maintenanceRecordTable,'ExpenditureIndex','Expenditure')
+    maintenanceRecordTable = """CREATE TABLE MaintenanceRecord (
+                    ServiceNumber Integer primary key, 
+                    CarVIN Integer, 
+                    DateOfService text, 
+                    Expenditure real, 
+                    ChargeToCustomer real, 
+                    NextServiceDate text
+                    )"""
 
-conn.close()
+    dbManager.createTable('Car', carsTable, 'CostIndex', 'PurchasePrice')
+    dbManager.createTable('Vendor', vendorTable)
+    dbManager.createTable('Sales', salesTable, 'SalesIndex', 'SalePrice')
+    dbManager.createTable('soldCars', soldTable)
+    dbManager.createTable('onHandCars', onHandTable)
+    dbManager.createTable('Staff', staffTable, 'PayIndex', 'Salary')
+    dbManager.createTable('MaintenanceRecord', maintenanceRecordTable, 'ExpenditureIndex', 'Expenditure')
+
+    dbManager.executeStatements()
+    dbManager.conn.close()
