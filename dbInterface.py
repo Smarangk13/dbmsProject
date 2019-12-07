379cfc6277e1959866765cf2089a62187eb451f0
Benjamin Mitchell <benjaminmitchell93@gmail.com>
2019-12-07 17:50:08 -0500

    Implement query 3

--- dbInterface.py
+++ dbInterface.py
@@ -18,8 +18,23 @@ class dbManager:
         self.cursor.execute(command)
         self.conn.commit()
 
+    def addEntryMaintenanceRecord(self, ServiceNumber, CarVIN, DateOfService, Expenditure, ChargeToCustomer, NextServiceDate):
+        command = f"INSERT INTO MaintenanceRecord VALUES ({ServiceNumber}, {CarVIN}, '{DateOfService}', {Expenditure}, {ChargeToCustomer}, '{NextServiceDate}');"
+        print(command)
+        self.cursor.execute(command)
+        self.conn.commit()
+
     def getTopSellingCar(self):
         command = "SELECT Model, COUNT(Model) FROM Car JOIN Sales ON Car.CarVIN = Sales.SCarVIN GROUP BY Model ORDER BY COUNT(Model);"
+        print(command)
+        self.cursor.execute(command)
+        row = self.cursor.fetchone()
+        print(row)
+        return row
+
+    # when is car x's next service date?
+    def getNextService(self, CarVIN):
+        command = "SELECT Car.CarVIN, NextServiceDate FROM Car JOIN MaintenanceRecord ON Car.CarVIN = MaintenanceRecord.CarVIN ORDER BY DateOfService DESC;"
         print(command)
         self.cursor.execute(command)
         row = self.cursor.fetchone()
@@ -30,4 +45,6 @@ if __name__ == '__main__':
     interface = dbManager()
     interface.addEntrySales(1, 1, 1, 'Joe Johnson', 1234567890, '1 Main Street', 23000.91)
     interface.addEntryCars(1, 'Range Rover', 'Land Rover', 2016, 'Black', 50000, 'V8', 2, 53000, '2019-11-08')
+    interface.addEntryMaintenanceRecord(1, 1, '2019-10-17', 80.0, 90.0, '2020-01-22')
     interface.getTopSellingCar()
+    interface.getNextService(1)
