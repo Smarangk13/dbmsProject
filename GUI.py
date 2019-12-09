e5a08fa7debba073618b45194cf61f7a3e225bf7
ghazalehJbr <gjabbari@bu.edu>
2019-12-09 18:38:56 -0500

    more data

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from DBMSProject.dbmsProject import dbInterface
+from dbmsProject import dbInterface
 
 
 class App(QMainWindow):
